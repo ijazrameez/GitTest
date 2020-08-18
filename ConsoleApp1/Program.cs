f51de3fa2f7aafcd77a64bf8e47b3bf6ce46b325
Ijaz mrm <ijaz_mrm@hotmail.com>
2020-08-18 18:24:59 +0530

    Edited code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             // code is edited
+            // edited code
             Console.WriteLine("Hello World!");
         }
     }
